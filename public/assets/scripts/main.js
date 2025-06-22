314c7d3504b727cbde7aabb1ec598da3e58d188f
Gilbert Alonso Huarcaya Matias <gilberthuarcaya@gmail.com>
2025-06-22 01:59:22 -0500

    feat: Add landing page for Beneficios with navigation and content sections

--- public/assets/scripts/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleButton = document.querySelector(".menu-toggle");
-  const menu = document.querySelector(".menu-horizontal");
-
-  toggleButton.addEventListener("click", () => {
-    menu.classList.toggle("show");
-  });
-});
